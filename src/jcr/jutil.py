4a9d3baffdbd261dc3ec703308a3c480f5df77aa
jchai.me <jreus@users.noreply.github.com>
2022-11-16 14:10:28 +0100

    updates to jutil

--- src/jcr/jutil.py
+++ src/jcr/jutil.py
@@ -286,7 +286,7 @@ def _logging_proc_main(pipe_rx, logger_name, log_level_console, log_level_file=N
     if using_transcript:
         transcript.close()
 
-    log.error("Exiting Logging Server Process")
+    log.warning("Exiting Logging Server Process")
 
 
 
@@ -347,8 +347,13 @@ class bcolors:
 
 
 
-# Used in argparse
+import threading
+import argparse
+
 def str2bool(v):
+    """
+    Type function for argparse to parse boolean flags
+    """
     if isinstance(v, bool):
         return v
     if v.lower() in ("yes", "true", "t", "y", "1"):
@@ -356,6 +361,41 @@ def str2bool(v):
     if v.lower() in ("no", "false", "f", "n", "0"):
         return False
     raise argparse.ArgumentTypeError("Boolean value expected.")
+
+
+class PeriodicTimer(object):
+    """
+    A periodic task running in threading.Timers
+    """
+
+    def __init__(self, interval, function, *args, **kwargs):
+        self._lock = threading.Lock()
+        self._timer = None
+        self.function = function
+        self.interval = interval
+        self.args = args
+        self.kwargs = kwargs
+        self._stopped = True
+        if kwargs.pop('autostart', True):
+            self.start()
+
+    def start(self, from_run=False):
+        self._lock.acquire()
+        if from_run or self._stopped:
+            self._stopped = False
+            self._timer = threading.Timer(self.interval, self._run)
+            self._timer.start()
+            self._lock.release()
+
+    def _run(self):
+        self.start(from_run=True)
+        self.function(*self.args, **self.kwargs)
+
+    def stop(self):
+        self._lock.acquire()
+        self._stopped = True
+        self._timer.cancel()
+        self._lock.release()
 
 
 # Used to broadcast parameter lists of potentially different lengths...
